c532efa9bcdc9b190a0df6967705d30c9573abea
GiuliaLeoni <leonig255@gmail.com>
2023-01-27 14:16:11 +0000

    sidebar placement

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -2,9 +2,8 @@ import { Filter } from "../Filter"
 
 export const SideBar = () => {
     return (
-        <>
-        <h1>sidebar</h1>
+        <section style={{backgroundColor: 'blue', width: '15rem', height: '100%', left: '0', top: '0', position: 'absolute', overflow: 'auto'}}>
         <Filter />
-        </>
+        </section>
     )
 }
